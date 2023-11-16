67440400ab90b69606effee23c94de62e368c91e
Egor Gaychukov <e.gaychukov@itransition.com>
2023-11-16 14:29:13 +0300

    Added a class library project to the solution

---
+++ ZoomMeeting/ZoomMeeting/Interfaces/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZoomMeeting.Interfaces
+{
+    internal interface ITokenService
+    {
+        (string AccessToken, string ResreshToken) GetTokenPair(string cliendId, string redirectUri);
+    }
+}
---
+++ ZoomMeeting/ZoomMeeting/Services/TokenService.cs
@@ -0,0 +1,20 @@
+﻿using ZoomMeeting.Interfaces;
+
+
+namespace ZoomMeeting.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TokenService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public (string AccessToken, string ResreshToken) GetTokenPair(string cliendId, string redirectUri)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
