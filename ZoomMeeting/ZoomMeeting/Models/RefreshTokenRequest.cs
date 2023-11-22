9d9a754abdb69696885f365b10a125e5054eefcb
Egor Gaychukov <e.gaychukov@itransition.com>
2023-11-22 17:15:50 +0300

    Implemented refreshing for access tokens

---
+++ ZoomMeeting/ZoomMeeting/Models/RefreshTokenRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ZoomMeeting.Models
+{
+    public class RefreshTokenRequest
+    {
+        public string? RefreshToken { get; set; }
+        public string? ClientId { get; set; }
+        public string? ClientSecret { get; set; }
+    }
+}
